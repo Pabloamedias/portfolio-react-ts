3cca7fa100779c4259a0c47443af1f8e00130131
Pabloamedias <p.olmedosaez@gmail.com>
2024-05-16 09:41:15 -0400

    Added an array of the projects and the images

---
+++ src/utils/actualProjects.ts
@@ -0,0 +1,31 @@
+export default [
+    {
+        id: 1,
+        title: 'Agendapp',
+        urlImage: 'public/images/agendapp.png',
+        description: 'Una aplicación tipo agenda que almacena tareas de formal local en el navegador.',
+        urlGithub: 'https://pabloamedias.github.io/Agendapp/'
+    },
+    {
+        id: 2,
+        title: 'Barbería Alura',
+        urlImage: 'public/images/barberia.png',
+        description: 'Una pagina sencilla sobre una barbería, se muestra datos como ubicación, servicios, precio, contacto, etc.',
+        urlGithub: 'https://pabloamedias.github.io/barberia/'
+    },
+    {
+        id: 3,
+        title: 'Formulario petShop',
+        urlImage: 'public/images/formulario_petshop.png',
+        description: 'Un formulario sencillo pero con distintas validaciones hechas con Regex y JS.',
+        urlGithub: 'https://pabloamedias.github.io/PetShop_validacion-de-formulario/'
+    },
+    {
+        id: 4,
+        title: 'Fruta & fruto',
+        urlImage: 'public/images/fruta&fruto.png',
+        description: 'Una pagina sobre recetas saludables, elaborada con Bootstrap 5.',
+        urlGithub: 'https://pabloamedias.github.io/Fruta-fruto/'
+    },
+]
+
