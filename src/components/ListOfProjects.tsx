d7da0487df67e58ceae6199db3690126bc15d5fe
Pabloamedias <p.olmedosaez@gmail.com>
2024-05-16 11:48:45 -0400

    Added new styles to the projects

--- src/components/ListOfProjects.tsx
+++ src/components/ListOfProjects.tsx
@@ -7,13 +7,12 @@ interface Props {
 
 const ListOfProjects = ({ projects }: Props) => {
   return (
-    <div className="container">
-        <div className="row">
-
-      {projects.map((project) => (
-        <Project key={project.id} project={project} />
-      ))}
-        </div>
+    <div className="container text-center">
+      <div className="row row row-cols-1 row-cols-md-3 mb-3">
+        {projects.map((project) => (
+          <Project key={project.id} project={project} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,16 +7,20 @@ interface Props {
 const Project = ({ project }: Props) => {
   return (
     <>
-      <div className="card col m-1 border-primary">
-        <img src={project.urlImage} className="card-img-top"  />
-        <div className="card-body">
-          <h5 className="card-title">{project.title}</h5>
-          <p className="card-text">
-     {project.description}
-          </p>
-          <a href={project.urlGithub} className="btn btn-primary  ">
-            Ir al proyecto
-          </a>
+      <div className="col mb-4">
+        <div className="card h-100">
+          <img src={project.urlImage} className="card-img-top" />
+          <div className="card-body">
+            <h5 className="card-title">{project.title}</h5>
+            <p className="card-text">{project.description}</p>
+            <a
+              href={project.urlGithub}
+              target="_blank"
+              className="btn btn-primary  "
+            >
+              Ir al proyecto
+            </a>
+          </div>
         </div>
       </div>
     </>
