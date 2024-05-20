e8350a1bb19957d6ea1cf97ee9470bcda08667e9
Pabloamedias <p.olmedosaez@gmail.com>
2024-05-20 11:36:15 -0400

    Change the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,30 +3,48 @@ import  "../assets/styles.css"
 
 const Navbar = () => {
   return (
-    <div className="navbar navbar-expand-lg bg-purple" data-bs-theme="dark">
-      <ul className="navbar-nav m-1">
-        <li className="nav-item ">
-          <NavLink className="nav-link" to={"/"}>
+<nav className="navbar navbar-expand-lg bg-purple" data-bs-theme="dark">
+        <div className="container-fluid m-1">
+          <NavLink className="navbar-brand" to={"/"}>
             Portafolio
           </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to={"/perfil"}>
-            Perfil
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to={"/contacto"}>
-            Contacto
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to={"/curriculo"}>
-            Curriculo
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to={"/"}>
+                  Mis Proyectos
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to={"/perfil"}>
+                  Perfil
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to={"#"}>
+                  Contacto
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#" target="_blank">
+                  Curriculo
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
   );
 };
 
