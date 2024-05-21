0662c9d516840be23c680f9b7874746cc6f63b87
Pabloamedias <p.olmedosaez@gmail.com>
2024-05-21 14:22:55 -0400

    Fix styles on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,9 @@ import "../assets/styles.css";
 
 const Footer = () => {
   return (
-    <footer className="bg-purple text-white text-center py-3 mt-auto">
+    <footer className="bg-purple text-white text-center py-2 mt-auto">
       <div className="container">
-        <h6>Esta pagina es un proyecto más de mi portafolios, revisa el codigo <a href="https://github.com/Pabloamedias/portfolio-react-ts">aquí</a></h6>
+        <h6>Esta pagina es un proyecto más de mi portafolios, revisa el codigo <a className="text-decoration-none" href="https://github.com/Pabloamedias/portfolio-react-ts" target="_blank">aquí</a></h6>
       </div>
     </footer>
   );
