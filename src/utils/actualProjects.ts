d0da0b3d8b4900770575345007d59978e1c5a7e3
Pabloamedias <p.olmedosaez@gmail.com>
2024-05-21 15:56:55 -0400

    Fix viteConfig, fix path for images

--- src/utils/actualProjects.ts
+++ src/utils/actualProjects.ts
@@ -2,7 +2,7 @@ export default [
     {
         id: 1,
         title: 'Agendapp',
-        urlImage: 'public/images/agendapp.png',
+        urlImage: '/images/agendapp.png',
         description: 'Una aplicación tipo agenda que almacena tareas de formal local en el navegador.',
         urlDeployed: 'https://pabloamedias.github.io/Agendapp/',
         urlGitHub: 'https://github.com/Pabloamedias/Agendapp'
@@ -10,7 +10,7 @@ export default [
     {
         id: 2,
         title: 'Barbería Alura',
-        urlImage: 'public/images/barberia.png',
+        urlImage: '/images/barberia.png',
         description: 'Una pagina sencilla sobre una barbería, se muestra datos como ubicación, servicios, precio, contacto, etc.',
         urlDeployed: 'https://pabloamedias.github.io/barberia/',
         urlGitHub: 'https://github.com/Pabloamedias/barberia'
@@ -18,7 +18,7 @@ export default [
     {
         id: 3,
         title: 'Formulario PetShop',
-        urlImage: 'public/images/formulario_petshop.png',
+        urlImage: '/images/formulario_petshop.png',
         description: 'Un formulario sencillo pero con distintas validaciones hechas con Regex y JS.',
         urlDeployed: 'https://pabloamedias.github.io/PetShop_validacion-de-formulario/',
         urlGitHub: 'https://github.com/Pabloamedias/PetShop_validacion-de-formulario/'
@@ -26,7 +26,7 @@ export default [
     {
         id: 4,
         title: 'Fruta & fruto',
-        urlImage: 'public/images/fruta&fruto.png',
+        urlImage: '/images/fruta&fruto.png',
         description: 'Una pagina sobre recetas saludables, elaborada con Bootstrap 5.',
         urlDeployed: 'https://pabloamedias.github.io/Fruta-fruto/',
         urlGitHub: 'https://github.com/Pabloamedias/Fruta-fruto/'
