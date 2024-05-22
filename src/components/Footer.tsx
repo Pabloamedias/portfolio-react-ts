7a3fe7c9fa793e6267fbe492e47bb97ba7aa1be7
Pabloamedias <p.olmedosaez@gmail.com>
2024-05-21 20:43:15 -0400

    fix typo in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-purple text-white text-center py-2 mt-auto">
       <div className="container">
-        <h6>Esta pagina es un proyecto más de mi portafolios, revisa el codigo <a className="text-decoration-none" href="https://github.com/Pabloamedias/portfolio-react-ts" target="_blank">aquí</a></h6>
+        <h6>Esta pagina es un proyecto más de mi portafolio, revisa el codigo <a className="text-decoration-none" href="https://github.com/Pabloamedias/portfolio-react-ts" target="_blank">aquí</a></h6>
       </div>
     </footer>
   );
